25d4af68da5f60ffcba35186d14c552c1ccc4e6e
JuanTrader500 <j.munoz12@utp.edu.co>
2025-07-21 18:57:32 -0500

    Add files via upload
    Actualizacion del codigo

--- ES_VIX_extraccion_csv.py
+++ ES_VIX_extraccion_csv.py
@@ -16,24 +16,30 @@ fecha_inicio = datetime(2005, 1, 1)
 try:
     # Descargar datos
     sp500_df = procesar_datos_yfinance(
-    yf.download("^GSPC", start=fecha_inicio, end=fecha_fin, progress=False),
+    yf.download("ES=F", start=fecha_inicio, end=fecha_fin, progress=False),
     "S&P 500")
+    
+    
     
     
     vix_df = procesar_datos_yfinance(
     yf.download('^VIX',start=fecha_inicio, end=fecha_fin, progress=False),
     'VIX')
-    vix_df = vix_df.iloc[:-1]
+    #vix_df = vix_df.iloc[:-1]
 
 except:
     print("Algo salio mal al crear los data frame")
+
+vix_df['vix_avg_hl'] = (vix_df['high'] + vix_df['low']) / 2
+sp500_df['sp_pct_move'] = (sp500_df['high'] - sp500_df['low']) / sp500_df['open'] * 100
    
 try:
     
     sp500_df.to_csv("C:/Users/juana/OneDrive/Escritorio/Datos_del_mercado/sp500_data_daily.csv",index=False)
-    vix_df.drop(vix_df.index[-1])
+    #vix_df.drop(vix_df.index[-1])
     vix_df.to_csv("C:/Users/juana/OneDrive/Escritorio/Datos_del_mercado/vix_data_daily.csv",index=False)
     
     print("Se exporto con exito el csv")
 except:
     print("No se pudo exportar el data_frame a un archivo csv")
+    
